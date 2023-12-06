28130a74d612509eeda3e682f16d21b4aaa1bde0
lachi1994 <60168655+lachi1994@users.noreply.github.com>
2023-12-06 08:35:59 +0530

    get user details

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"laxmi",
+            hobby:searching,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
 
 app.listen(8000,() => {
     console.log("listening to 8000");
